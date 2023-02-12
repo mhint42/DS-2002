3c6c165603b2ba2b0e93986f127a63d67ae1700e
Mary Hinton <124236251+mhint42@users.noreply.github.com>
2023-02-12 19:53:12 +0000

    Add files via upload

---
+++ Lab_02b_Datawarehouse_Table_Inserts.sql
@@ -0,0 +1,217 @@
+-- --------------------------------------------------------------------------------------------------------------
+-- TODO: Extract the appropriate data from the northwind database, and INSERT it into the Northwind_DW database.
+-- --------------------------------------------------------------------------------------------------------------
+
+-- ----------------------------------------------
+-- Populate dim_customers
+-- ----------------------------------------------
+INSERT INTO `northwind_dw`.`dim_customers`
+(`customer_key`,
+`company`,
+`last_name`,
+`first_name`,
+`job_title`,
+`business_phone`,
+`fax_number`,
+`address`,
+`city`,
+`state_province`,
+`zip_postal_code`,
+`country_region`)
+SELECT `id`,
+`company`,
+`last_name`,
+`first_name`,
+`job_title`,
+`business_phone`,
+`fax_number`,
+`address`,
+`city`,
+`state_province`,
+`zip_postal_code`,
+`country_region`
+FROM northwind.customers;
+
+-- ----------------------------------------------
+-- Validate that the Data was Inserted ----------
+-- ----------------------------------------------
+SELECT * FROM northwind_dw.dim_customers;
+
+
+-- ----------------------------------------------
+-- Populate dim_employees
+-- ----------------------------------------------
+INSERT INTO `northwind_dw`.`dim_employees`
+(`employee_key`,
+`company`,
+`last_name`,
+`first_name`,
+`email_address`,
+`job_title`,
+`business_phone`,
+`home_phone`,
+`fax_number`,
+`address`,
+`city`,
+`state_province`,
+`zip_postal_code`,
+`country_region`,
+`web_page`)
+SELECT `employees`.`id`,
+    `employees`.`company`,
+    `employees`.`last_name`,
+    `employees`.`first_name`,
+    `employees`.`email_address`,
+    `employees`.`job_title`,
+    `employees`.`business_phone`,
+    `employees`.`home_phone`,
+    `employees`.`fax_number`,
+    `employees`.`address`,
+    `employees`.`city`,
+    `employees`.`state_province`,
+    `employees`.`zip_postal_code`,
+    `employees`.`country_region`,
+    `employees`.`web_page`
+FROM `northwind`.`employees`;
+
+-- ----------------------------------------------
+-- Validate that the Data was Inserted ----------
+-- ----------------------------------------------
+SELECT * FROM northwind_dw.dim_employees;
+
+
+-- ----------------------------------------------
+-- Populate dim_products
+-- ----------------------------------------------
+INSERT INTO `northwind_dw`.`dim_products`
+(`product_key`,
+`product_code`,
+`product_name`,
+`standard_cost`,
+`list_price`,
+`reorder_level`,
+`target_level`,
+`quantity_per_unit`,
+`discontinued`,
+`minimum_reorder_quantity`,
+`category`)
+# TODO: Write a SELECT Statement to Populate the table;
+SELECT `products`.`id`,
+    `products`.`product_code`,
+    `products`.`product_name`,
+    `products`.`standard_cost`,
+    `products`.`list_price`,
+    `products`.`reorder_level`,
+    `products`.`target_level`,
+    `products`.`quantity_per_unit`,
+    `products`.`discontinued`,
+    `products`.`minimum_reorder_quantity`,
+    `products`.`category`
+FROM `northwind`.`products`;
+
+-- ----------------------------------------------
+-- Validate that the Data was Inserted ----------
+-- ----------------------------------------------
+SELECT * FROM northwind_dw.dim_products;
+
+
+-- ----------------------------------------------
+-- Populate dim_shippers
+-- ----------------------------------------------
+INSERT INTO `northwind_dw`.`dim_shippers`
+(`shipper_key`,
+`company`,
+`address`,
+`city`,
+`state_province`,
+`zip_postal_code`,
+`country_region`)
+# TODO: Write a SELECT Statement to Populate the table;
+SELECT `shippers`.`id`,
+    `shippers`.`company`,
+	`shippers`.`address`,
+	`shippers`.`city`,
+    `shippers`.`state_province`,
+	`shippers`.`zip_postal_code`,
+	`shippers`.`country_region`
+FROM `northwind`.`shippers`;
+
+
+-- ----------------------------------------------
+-- Validate that the Data was Inserted ----------
+-- ----------------------------------------------
+SELECT * FROM northwind_dw.dim_shippers;
+
+
+
+-- ----------------------------------------------
+-- Populate fact_orders
+-- ----------------------------------------------
+INSERT INTO `northwind_dw`.`fact_orders`
+(`order_key`,
+`employee_key`,
+`customer_key`,
+`product_key`,
+`shipper_key`,
+`ship_name`,
+`ship_address`,
+`ship_city`,
+`ship_state_province`,
+`ship_zip_postal_code`,
+`ship_country_region`,
+`quantity`,
+`order_date`,
+`shipped_date`,
+`unit_price`,
+`discount`,
+`shipping_fee`,
+`taxes`,
+`payment_type`,
+`paid_date`,
+`tax_rate`,
+`order_status`,
+`order_details_status`)
+/* 
+--------------------------------------------------------------------------------------------------
+TODO: Write a SELECT Statement that:
+- JOINS the northwind.orders table with the northwind.orders_status table
+- JOINS the northwind.orders with the northwind.order_details table.
+--  (TIP: Remember that there is a one-to-many relationship between orders and order_details).
+- JOINS the northwind.order_details table with the northwind.order_details_status table.
+--------------------------------------------------------------------------------------------------
+- The column list I've included in the INSERT INTO clause above should be your guide to which 
+- columns you're required to extract from each of the four tables. Pay close attention!
+--------------------------------------------------------------------------------------------------
+*/
+SELECT northwind.orders.id,
+`employee_id`,
+`customer_id`,
+northwind.order_details.product_id,
+`shipper_id`,
+`ship_name`,
+`ship_address`,
+`ship_city`,
+`ship_state_province`,
+`ship_zip_postal_code`,
+`ship_country_region`,
+`quantity`,
+`order_date`,
+`shipped_date`,
+`unit_price`,
+`discount`,
+`shipping_fee`,
+`taxes`,
+`payment_type`,
+`paid_date`,
+`tax_rate`,
+northwind.orders_status.id,
+northwind.order_details_status.id
+FROM northwind.orders
+JOIN northwind.orders_status ON northwind.orders.status_id=northwind.orders_status.id
+JOIN northwind.order_details ON northwind.orders.id=northwind.order_details.order_id
+JOIN northwind.order_details_status ON northwind.order_details.status_id=northwind.order_details_status.id;
+
+-- ----------------------------------------------
+-- Validate that the Data was Inserted ----------
+-- ----------------------------------------------
+SELECT * FROM northwind_dw.fact_orders;
